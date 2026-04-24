c69dcd2c0d23c58377f9e657722a92341dad0fc0
Fei Wang <wf199209@gmail.com>
2026-04-24 11:48:58 -0400

    code line 2

---
+++ vscode_learning.py
@@ -0,0 +1,18 @@
+print("hello world!")
+
+
+import torch
+
+# 检查是否能用 Apple Silicon 的 GPU 加速 (MPS)
+device = "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"当前设备: {device}")
+
+# 创建一个简单的张量进行测试
+x = torch.rand(5, 4).to(device)
+print(x)
+
+
+# this is a test code for vscode notebook
+# import numpy as np
+# new code line 1
+# new code line 2
